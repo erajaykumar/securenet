6531285a2c31117ab66cca5e3c88bb6bd04002bb
neha kumari <nehakumari199460@gmail.com>
2023-05-19 14:51:32 +0530

    signal-r-initial-checkin

---
+++ SignalRProject/Hubs/ConnectedHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRProject.Hubs
+{
+    public class ConnectedHub:Hub
+    {
+       
+        public async Task SendMessage( string message)
+        {
+            
+                await Clients.All.SendAsync("ReceiveMessage", message);
+            
+        }
+
+    }
+}
---
+++ SignalRProject/wwwroot/js/clientHub.js
@@ -0,0 +1,25 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/ConnectedHub").build();
+
+connection.on("ReceiveMessage", function (message) {
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+
+    li.textContent = ` user says ${message}`;
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+ 
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage",message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
